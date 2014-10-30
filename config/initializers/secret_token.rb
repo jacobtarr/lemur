a0c2843c3f597c1c9a19d8d240be0f5fcd37f474
Jacob Tarr <jtarr.rams@gmail.com>
2014-10-29 20:50:57 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lemur::Application.config.secret_key_base = 'af89b86d8edd49613a4731018685971c227039bb5e5d3f24c3e8073726e9afa68e355548f5977ccc4bade0dcd8dab105c34399289eac474d3a155b234d2e96ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lemur::Application.config.session_store :cookie_store, key: '_lemur_session'
